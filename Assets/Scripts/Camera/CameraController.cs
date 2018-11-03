ce6e72d31b73cf1cc5c9cae6acd14295f9c2d604
Peter Brendel <peter_brendel@hotmail.com>
2018-11-03 18:21:24 -0300

    First commit

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float cameraSpeed = 120.0f;
+    public GameObject toFollow;
+    public float upperClamp = 80.0f;
+    public float lowerClamp = -80.0f;
+    public float inputSens = 150.0f;
+    private float distanceX;
+    private float distanceY;
+    private float distanceZ;
+    private float rotationY;
+    private float rotationX;
+    private float smoothinX;
+    private float smoothinY;
+    private float usrinputX;
+    private float usrinputY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotationY = rotation.y;
+        rotationX = rotation.x;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            usrinputX = Input.GetAxis("Mouse Y");
+            usrinputY = Input.GetAxis("Mouse X");
+            rotationX += usrinputX * inputSens * Time.deltaTime;
+            rotationY += usrinputY * inputSens * Time.deltaTime;
+            rotationX = Mathf.Clamp(rotationX, lowerClamp, upperClamp);
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Transform target = toFollow.transform;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, cameraSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/CharController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    public float speed = 10.0f;
+    private CharacterController cc;
+    public GameObject camera;
+    public float gravity = 10.0f;
+    public float rotationSpeed = 120.0f;
+    public float jumpPower = 10.0f;
+    private float beginJump = 0.0f;
+    private float acc = 1f;
+    private Vector3 toLook;
+
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        cc.transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0) * Time.deltaTime * rotationSpeed);
+        cc.Move(cc.transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+        //Begin A: If Mouse2 is pressed then make player rotation follow camera
+        if (Input.GetMouseButton(1))
+        {
+            toLook = camera.transform.forward;
+            toLook.y = 0;
+            if (toLook.sqrMagnitude != 0.0f)
+                cc.transform.LookAt(cc.transform.position + toLook);
+            cc.Move(cc.transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        }
+        //End A.
+
+        if (Time.time - beginJump > 0.27f) {
+            cc.Move(new Vector3(0, -gravity * Time.deltaTime * acc, 0));
+            acc += 0.04f;
+        }
+        else
+        {
+            cc.Move(new Vector3(0, gravity * Time.deltaTime * jumpPower * acc, 0));
+            acc += 0.01f;
+        }
+
+        if (cc.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            beginJump = Time.time;
+            acc = 1f;
+        }
+
+        //Debug.Log(cc.transform.position);
+
+    }
+}
